74c9a3d6f4f57fbb4ac9246811caa80cf1aa5542
Gabriel Fior <fior.gbm@gmail.com>
2022-11-07 13:00:14 +0100

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,39 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import 'antd/dist/antd.css';
+import 'react-toastify/dist/ReactToastify.css';
+import type { AppProps } from 'next/app';
+import { WagmiConfig, createClient, chain  } from "wagmi";
+import { ConnectKitProvider, ConnectKitButton, getDefaultClient } from "connectkit";
+import { ToastContainer } from 'react-toastify';
+
+const alchemyId = process.env.ALCHEMY_API_KEY;
+
+const client = createClient(
+  getDefaultClient({
+    appName: "Money Streamer",
+    alchemyId,
+    chains: [chain.mainnet, chain.polygon, chain.optimism, chain.goerli, chain.hardhat],
+  }),
+);
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig client={client}>
+      <ConnectKitProvider theme="auto" mode="dark">
+      <div
+      style={{
+        display: 'flex',
+        alignItems: 'right',
+        justifyContent: 'right',
+        margin: '10px'
+      }}
+    >
+      <ConnectKitButton showBalance={true} />
+      </div>
+        
+        <Component {...pageProps} />
+      </ConnectKitProvider>
+      <ToastContainer />
+    </WagmiConfig>
+  );
 }
